69fee033bf4935b83a40ec43fd2c6468ca34d5c8
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2024-03-09 15:19:00 +0530

    sidebar functionality made

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Conversation from './Conversation'
-
+import React from "react";
+import Conversation from "./Conversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 
 export default function Conversations() {
+  const { loading, conversation } = useGetConversations();
+
   return (
-    <div className='py-2 flex flex-col overflow-auto'>
- 			<Conversation />
- 			<Conversation />
- 			<Conversation />
- 			<Conversation />
- 			<Conversation />
- 			<Conversation />
- 		</div>
-  )
+    <div className="py-2 flex flex-col overflow-auto">
+      {conversation.map((conversation, index) => (
+          <Conversation
+            key={conversation._id}
+            conversation={conversation}
+            emoji={getRandomEmoji()}
+            lastIdx={index === conversation.length - 1}
+          />
+        ))}
+		{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+    </div>
+  );
 }
