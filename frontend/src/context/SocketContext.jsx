d99d11905e4179b1665cc46202cbc5461ccb0f63
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2024-03-15 01:29:50 +0530

    SocketContext updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if (authUser){
-            const socket = io("http://localhost:8000", {
+            const socket = io("https://chat-io-qegy.onrender.com", {
                 query:{
                     userId: authUser._id,
                 }
