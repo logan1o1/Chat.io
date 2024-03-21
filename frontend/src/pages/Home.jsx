513f43b3c8efd3d1bf99eac5e76ebcfdffd2203a
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2024-03-21 22:14:39 +0530

    SEO handling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import MessageContainer from '../components/messages/MessageContainer'
 
 export default function Home() {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <div className='flex sm:h-[450px] sm:w-3/5 md:h-[550px] md:w-4/5 rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <Sidebar/>
       <MessageContainer/>
     </div>
