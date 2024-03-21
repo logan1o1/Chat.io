64fd3fa2138445b33f9b44b920023ba349b84aaf
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2024-03-21 22:59:53 +0530

    nvm

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Sidebar from '../components/sidebar/Sidebar'
 import MessageContainer from '../components/messages/MessageContainer'
-import { ImMenu } from "react-icons/im";
 
 export default function Home() {
-  const [showSidebar, setShowSidebar] = useState(true);
-
-  const toggleSidebar = () => {
-    setShowSidebar(!showSidebar);
-  }
-
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-      <ImMenu onClick={toggleSidebar}/>
-      {showSidebar && <Sidebar/>}
+      <Sidebar/>
       <MessageContainer/>
     </div>
   )
