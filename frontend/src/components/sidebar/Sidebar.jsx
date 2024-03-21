870ace54f66ca01ea168e4526ce7b1c56f2b1696
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2024-03-21 22:48:15 +0530

    sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import LogoutButton from "./LogoutButton";
 
 export default function Sidebar() {
   return (
-    <div className="border-r border-slate-500 p-4 flex flex-col">
+    <div className="border-r md:hidden border-slate-500 p-4 flex flex-col">
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/sidebar/Sidebar'
 import MessageContainer from '../components/messages/MessageContainer'
+import { ImMenu } from "react-icons/im";
 
 export default function Home() {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  }
+
   return (
-    <div className='flex sm:h-[450px] sm:w-3/5 md:h-[550px] md:w-4/5 rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-      <Sidebar/>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <ImMenu onClick={toggleSidebar}/>
+      {showSidebar && <Sidebar/>}
       <MessageContainer/>
     </div>
   )
